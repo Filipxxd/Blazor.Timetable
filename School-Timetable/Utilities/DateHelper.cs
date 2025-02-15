1512377983d7bda4658138c54e22ffc62e97ce6a
filip <filip.chromecka@outlook.com>
2025-02-15 19:59:55 +0100

    Add DateHelper utility

---
+++ School-Timetable/Utilities/DateHelper.cs
@@ -0,0 +1,6 @@
+﻿namespace School_Timetable.Utilities;
+
+internal static class DateHelper
+{
+    public static DateTime GetStartOfWeekDate(DateTime dt, DayOfWeek firstDayOfWeek) => dt.AddDays(-(int)dt.DayOfWeek + (int)firstDayOfWeek); 
+}
