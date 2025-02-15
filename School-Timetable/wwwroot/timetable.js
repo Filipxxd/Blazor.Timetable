d58819914d70c4543b0a38592e6acc96db346ede
filip <filip.chromecka@outlook.com>
2025-02-15 20:00:50 +0100

    Single script ref

---
+++ School-Timetable/wwwroot/timetable.js
@@ -0,0 +1,8 @@
+﻿const script = document.createElement('script');
+script.src = '_content/School-Timetable/interact.min.js';
+script.onload = function() {
+    const customScript = document.createElement('script');
+    customScript.src = '_content/School-Timetable/dragDrop.js';
+    document.body.appendChild(customScript);
+};
+document.body.appendChild(script);
