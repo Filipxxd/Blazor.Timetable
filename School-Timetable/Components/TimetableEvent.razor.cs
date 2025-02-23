6e2cfcc404475b2e0eeeff9e981fdb8880b363fe
filip <filip.chromecka@outlook.com>
2025-02-23 21:52:48 +0100

    Refactor timetable display

--- School-Timetable/Components/TimetableEvent.razor.cs
+++ School-Timetable/Components/TimetableEvent.razor.cs
@@ -8,6 +8,8 @@ public partial class TimetableEvent
     
     [Parameter] public Guid EventId { get; set; }
     [Parameter] public string Title { get; set; } = default!;
+    [Parameter] public int Span { get; set; }
+    [Parameter] public bool IsWholeDay { get; set; }
     [Parameter] public RenderFragment DetailTemplate { get; set; } = default!; 
     
     private void TogglePopover()
