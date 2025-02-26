1d7e339c2eed5d721007f7d49f776adf3c1233c6
filip <filip.chromecka@outlook.com>
2025-02-26 20:45:55 +0100

    Add style config class

---
+++ School-Timetable/Configuration/StyleConfig.cs
@@ -0,0 +1,10 @@
+﻿namespace School_Timetable.Configuration;
+
+public sealed class StyleConfig
+{
+    public string PrimaryColor { get; set; } = default!;
+    public string SecondaryColor { get; set; } = default!;
+    public string BackgroundColor { get; set; } = default!;
+    public string TextColor { get; set; } = default!;
+    public string GridBorderColor { get; set; } = default!;
+}
