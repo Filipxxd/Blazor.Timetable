787e879dc8e45ce19c780ebfe387b76b8ea410e4
filip <filip.chromecka@outlook.com>
2025-03-06 22:13:20 +0100

    Fix method logic

--- Timetable/Utilities/DateHelper.cs
+++ Timetable/Utilities/DateHelper.cs
@@ -2,5 +2,13 @@
 
 internal static class DateHelper
 {
-    public static DateTime GetStartOfWeekDate(DateTime dt, DayOfWeek firstDayOfWeek) => dt.AddDays(-(int)dt.DayOfWeek + (int)firstDayOfWeek); //TODO: if from wednesday to tuesday, calc approprietly
+    public static DateTime GetStartOfWeekDate(DateTime dt, DayOfWeek firstDayOfWeek)
+    {
+        var currentDayOfWeek = (int)dt.DayOfWeek;
+        var targetDayOfWeek = (int)firstDayOfWeek;
+
+        var diff = (7 + (currentDayOfWeek - targetDayOfWeek)) % 7;
+
+        return dt.AddDays(-diff).Date;
+    }
 }
