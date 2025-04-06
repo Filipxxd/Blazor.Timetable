0d375b89e41c68d1bcb5e7006ffa9b3853402baa
filip <filip.chromecka@outlook.com>
2025-04-06 12:43:36 +0200

    Add readonly modifier

--- Timetable/Components/TimetableEvent.razor.cs
+++ Timetable/Components/TimetableEvent.razor.cs
@@ -2,68 +2,67 @@
 using Microsoft.AspNetCore.Components.Web;
 using System.Diagnostics;
 using Timetable.Common.Enums;
-using Timetable.Structure;
 
 namespace Timetable.Components;
 
 public partial class TimetableEvent
 {
-	private const int MousedownThreshold = 500;
-	private Stopwatch _clickStopwatch = new();
-	private bool _popoverVisible = false;
-	private PopupState _state = PopupState.Detail;
+    private const int MousedownThreshold = 500;
+    private readonly Stopwatch _clickStopwatch = new();
+    private bool _popoverVisible = false;
+    private PopupState _state = PopupState.Detail;
 
-	[Parameter] public Guid EventId { get; set; }
-	[Parameter] public string Title { get; set; } = default!;
-	[Parameter] public int Span { get; set; }
-	[Parameter] public bool IsWholeDay { get; set; }
-	[Parameter] public RenderFragment DetailTemplate { get; set; } = default!;
-	[Parameter] public RenderFragment EditTemplate { get; set; } = default!;
-	[Parameter] public RenderFragment DeleteTemplate { get; set; } = default!;
+    [Parameter] public Guid EventId { get; set; }
+    [Parameter] public string Title { get; set; } = default!;
+    [Parameter] public int Span { get; set; }
+    [Parameter] public bool IsWholeDay { get; set; }
+    [Parameter] public RenderFragment DetailTemplate { get; set; } = default!;
+    [Parameter] public RenderFragment EditTemplate { get; set; } = default!;
+    [Parameter] public RenderFragment DeleteTemplate { get; set; } = default!;
 
-	private void OnMouseDown(MouseEventArgs e)
-	{
-		if (e.Button == 0)
-		{
-			_clickStopwatch.Restart();
-		}
-	}
+    private void OnMouseDown(MouseEventArgs e)
+    {
+        if (e.Button == 0)
+        {
+            _clickStopwatch.Restart();
+        }
+    }
 
-	private void OnMouseUp(MouseEventArgs e)
-	{
-		if (e.Button != 0 || !_clickStopwatch.IsRunning) return;
-		_clickStopwatch.Stop();
+    private void OnMouseUp(MouseEventArgs e)
+    {
+        if (e.Button != 0 || !_clickStopwatch.IsRunning) return;
+        _clickStopwatch.Stop();
 
-		if (_clickStopwatch.ElapsedMilliseconds < MousedownThreshold)
-		{
-			TogglePopover();
-		}
-	}
+        if (_clickStopwatch.ElapsedMilliseconds < MousedownThreshold)
+        {
+            TogglePopover();
+        }
+    }
 
-	private void TogglePopover()
-	{
-		_state = PopupState.Detail;
-		_popoverVisible = !_popoverVisible;
-		StateHasChanged();
-	}
+    private void TogglePopover()
+    {
+        _state = PopupState.Detail;
+        _popoverVisible = !_popoverVisible;
+        StateHasChanged();
+    }
 
-	private void ToggleEdit()
-	{
-		_state = PopupState.Edit;
-	}
+    private void ToggleEdit()
+    {
+        _state = PopupState.Edit;
+    }
 
-	private void ToggleDelete()
-	{
-		_state = PopupState.Delete;
-	}
+    private void ToggleDelete()
+    {
+        _state = PopupState.Delete;
+    }
 
-	private void EditEvent()
-	{
-		// Edit event logic
-	}
+    private void EditEvent()
+    {
+        // Edit event logic
+    }
 
-	private void DeleteEvent()
-	{
-		// Delete event logic
-	}
+    private void DeleteEvent()
+    {
+        // Delete event logic
+    }
 }
