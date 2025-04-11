0b506eb76db66eea861669a497c8164c3a4fdb18
filip <filip.chromecka@outlook.com>
2025-04-11 18:09:48 +0200

    Add documentation

--- Timetable/Configuration/ExportConfig.cs
+++ Timetable/Configuration/ExportConfig.cs
@@ -5,16 +5,27 @@ namespace Timetable.Configuration;
 
 public sealed class ExportConfig<TEvent> where TEvent : class
 {
-	public required string FileName { get; init; }
-	public required ITransformer Transformer { get; init; }
+    /// <summary>
+    /// File name for the export. This is the name of the file that will be created.
+    /// </summary>
+    public required string FileName { get; init; }
+
+    /// <summary>
+    /// Transformer to use for the export. This is the class that will handle the transformation of the data.
+    /// </summary>
+    public required ITransformer Transformer { get; init; }
+
+    /// <summary>
+    /// Properties to export. This is a list of properties that will be included in the export.
+    /// </summary>
 	public IList<INamePropertySelector<TEvent>> Properties { get; init; } = [];
 
-	internal void Validate()
-	{
-		if (!Properties.Any())
-			throw new InvalidSetupException("At least one property must be provided.");
+    internal void Validate()
+    {
+        if (!Properties.Any())
+            throw new InvalidSetupException("At least one property must be provided.");
 
-		if (string.IsNullOrWhiteSpace(FileName))
-			throw new InvalidSetupException("FileName must be provided.");
-	}
+        if (string.IsNullOrWhiteSpace(FileName))
+            throw new InvalidSetupException("FileName must be provided.");
+    }
 }
