9e9680b63377aa12fd51aa9043f643599d2f408b
filip <filip.chromecka@outlook.com>
2025-04-20 00:01:34 +0200

    Add code behind files

---
+++ Timetable/Components/Shared/Forms/Dropdown.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Timetable.Components.Shared.Forms;
+
+public partial class Dropdown<TType>
+{
+    [Parameter, EditorRequired] public TType[] Options { get; set; } = [];
+    [Parameter, EditorRequired] public TType Value { get; set; } = default!;
+    [Parameter, EditorRequired] public EventCallback<TType> ValueChanged { get; set; }
+    [Parameter, EditorRequired] public string Label { get; set; } = default!;
+    [Parameter] public string? ErrorMessage { get; set; }
+
+    private TType BoundValue
+    {
+        get => Value;
+        set
+        {
+            if (EqualityComparer<TType>.Default.Equals(Value, value)) return;
+
+            Value = value;
+            ValueChanged.InvokeAsync(value);
+        }
+    }
+}
