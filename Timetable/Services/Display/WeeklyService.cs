6117fbf1bd092e9ac43bc3bd79b4fc96db3d16bc
filip <filip.chromecka@outlook.com>
2025-04-19 17:46:09 +0200

    Simplify

--- Timetable/Services/Display/WeeklyService.cs
+++ Timetable/Services/Display/WeeklyService.cs
@@ -3,77 +3,76 @@ using Timetable.Common.Helpers;
 using Timetable.Configuration;
 using Timetable.Structure;
 
-namespace Timetable.Services.Display
+namespace Timetable.Services.Display;
+
+internal sealed class WeeklyService
 {
-    internal sealed class WeeklyService
+    public Grid<TEvent> CreateGrid<TEvent>(
+        IList<TEvent> events,
+        TimetableConfig config,
+        DateTime currentDate,
+        CompiledProps<TEvent> props) where TEvent : class
     {
-        public Grid<TEvent> CreateGrid<TEvent>(
-            IList<TEvent> events,
-            TimetableConfig config,
-            DateTime currentDate,
-            CompiledProps<TEvent> props) where TEvent : class
+        var cellDates = CalculateGridDates(currentDate, config.Days).ToList();
+
+        var startDate = cellDates.First();
+        var gridEndDate = cellDates.Last();
+
+        var weeklyEvents = events.Where(e =>
         {
-            var cellDates = CalculateGridDates(currentDate, config.Days).ToList();
+            var eventStart = props.GetDateFrom(e);
+            return eventStart >= startDate && eventStart < gridEndDate.AddDays(1);
+        }).ToList();
 
-            var startDate = cellDates.First();
-            var gridEndDate = cellDates.Last();
+        var rowTitles = config.Hours.Select(hour =>
+            config.Is24HourFormat
+                ? TimeSpan.FromHours(hour).ToString(@"hh\:mm")
+                : DateTime.Today.AddHours(hour).ToString("h tt")
+        ).ToList();
 
-            var weeklyEvents = events.Where(e =>
+        var dayIndex = 1;
+        var columns = new List<Column<TEvent>>();
+        foreach (var cellDate in cellDates)
+        {
+            var column = new Column<TEvent>
             {
-                var eventStart = props.GetDateFrom(e);
-                return eventStart >= startDate && eventStart < gridEndDate.AddDays(1);
-            }).ToList();
-
-            var rowTitles = config.Hours.Select(hour =>
-                config.Is24HourFormat
-                    ? TimeSpan.FromHours(hour).ToString(@"hh\:mm")
-                    : DateTime.Today.AddHours(hour).ToString("h tt")
-            ).ToList();
-
-            var dayIndex = 1;
-            var columns = new List<Column<TEvent>>();
-            foreach (var cellDate in cellDates)
-            {
-                var column = new Column<TEvent>
-                {
-                    DayOfWeek = cellDate.DayOfWeek,
-                    Index = dayIndex,
-                    Cells = TimetableHelper.CreateCells(cellDate, config, weeklyEvents, props)
-                };
-                columns.Add(column);
-                dayIndex++;
-            }
-
-            return new Grid<TEvent>
-            {
-                Title = $"{startDate:dddd d. MMMM} - {gridEndDate:dddd d. MMMM yyyy}".CapitalizeWords(),
-                RowTitles = rowTitles,
-                Columns = columns
+                DayOfWeek = cellDate.DayOfWeek,
+                Index = dayIndex,
+                Cells = TimetableHelper.CreateCells(cellDate, config, weeklyEvents, props)
             };
+            columns.Add(column);
+            dayIndex++;
         }
 
-        private static IEnumerable<DateTime> CalculateGridDates(DateTime currentDate, IEnumerable<DayOfWeek> configuredDays)
+        return new Grid<TEvent>
         {
-            var orderedDays = configuredDays.OrderBy(d => d).ToList();
-            if (orderedDays.Count == 0)
-                throw new InvalidOperationException();
+            Title = $"{startDate:dddd d. MMMM} - {gridEndDate:dddd d. MMMM yyyy}".CapitalizeWords(),
+            RowTitles = rowTitles,
+            Columns = columns
+        };
+    }
 
-            var startDate = DateHelper.GetStartOfWeekDate(currentDate, orderedDays.First());
-            var dates = new List<DateTime> { startDate };
-            var previousDate = startDate;
-            var previousDayValue = (int)orderedDays.First();
+    private static IEnumerable<DateTime> CalculateGridDates(DateTime currentDate, IEnumerable<DayOfWeek> configuredDays)
+    {
+        var orderedDays = configuredDays.OrderBy(d => d).ToList();
+        if (orderedDays.Count == 0)
+            throw new InvalidOperationException();
 
-            foreach (var day in orderedDays.Skip(1))
-            {
-                var diff = ((int)day - previousDayValue + 7) % 7;
-                diff = diff == 0 ? 7 : diff;
-                var nextDate = previousDate.AddDays(diff);
-                dates.Add(nextDate);
-                previousDayValue = (int)day;
-                previousDate = nextDate;
-            }
+        var startDate = DateHelper.GetStartOfWeekDate(currentDate, orderedDays.First());
+        var dates = new List<DateTime> { startDate };
+        var previousDate = startDate;
+        var previousDayValue = (int)orderedDays.First();
 
-            return dates;
+        foreach (var day in orderedDays.Skip(1))
+        {
+            var diff = ((int)day - previousDayValue + 7) % 7;
+            diff = diff == 0 ? 7 : diff;
+            var nextDate = previousDate.AddDays(diff);
+            dates.Add(nextDate);
+            previousDayValue = (int)day;
+            previousDate = nextDate;
         }
+
+        return dates;
     }
 }
