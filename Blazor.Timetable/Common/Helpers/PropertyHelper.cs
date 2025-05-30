b283abb36cc38184216c117cd039356e004482db
filip <filip.chromecka@outlook.com>
2025-05-30 22:52:35 +0200

    Cleanup

--- Blazor.Timetable/Common/Helpers/PropertyHelper.cs
+++ Blazor.Timetable/Common/Helpers/PropertyHelper.cs
@@ -10,11 +10,14 @@ internal static class PropertyHelper
     {
         if (expression.Body is not MemberExpression
             && expression.Body is not UnaryExpression { Operand: MemberExpression })
-            throw new ArgumentException("Expression must point to a property", nameof(expression));
+            throw new ArgumentException(
+                "Expression must point to a property", nameof(expression));
 
         var compiledExpression = expression.Compile();
 
-        return obj => Equals(obj, default(TObject)) ? default : compiledExpression(obj);
+        return obj => Equals(obj, default(TObject))
+            ? default
+            : compiledExpression(obj);
     }
 
     public static Action<TObject, TProperty?> CreateSetter<TObject, TProperty>
@@ -24,11 +27,13 @@ internal static class PropertyHelper
         {
             MemberExpression me => me,
             UnaryExpression ue when ue.Operand is MemberExpression me => me,
-            _ => throw new ArgumentException("Expression must point to a property", nameof(expression)),
+            _ => throw new ArgumentException(
+                "Expression must point to a property", nameof(expression)),
         };
 
         if (memberExpr.Member is not PropertyInfo propertyInfo)
-            throw new ArgumentException("Member is not a property", nameof(expression));
+            throw new ArgumentException(
+                "Member is not a property", nameof(expression));
 
         var parameter = expression.Parameters[0];
         var valueParameter = Expression.Parameter(typeof(TProperty), "value");
@@ -36,11 +41,13 @@ internal static class PropertyHelper
         var valueToAssign = valueParameter as Expression;
 
         if (propertyInfo.PropertyType != typeof(TProperty))
-            valueToAssign = Expression.Convert(valueParameter, propertyInfo.PropertyType);
+            valueToAssign = Expression.Convert(
+                valueParameter, propertyInfo.PropertyType);
 
         var member = Expression.MakeMemberAccess(parameter, propertyInfo);
         var assign = Expression.Assign(member, valueToAssign);
-        var lambda = Expression.Lambda<Action<TObject, TProperty?>>(assign, parameter, valueParameter);
+        var lambda = Expression.Lambda<Action<TObject, TProperty?>>(
+            assign, parameter, valueParameter);
 
         return lambda.Compile();
     }
